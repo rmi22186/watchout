0fcdd5b9ffaf9c98e179528e5e414e639f8fc09f
rmi22186 <rmi22186@gmail.com>
2014-12-19 17:03:42 -0800

    Enemies moving randomly

--- enemy.js
+++ enemy.js
@@ -1,8 +1,11 @@
-var Enemy = function(x, y, image) {
-  this.x = x;
-  this.y = y;
+var Enemy = function(id, x, y, image, canvasW, canvasH) {
+  this._canvasW = canvasW || 500;
+  this._canvasH = canvasH || 500;
+  this.id = id;
   this._w = 50;
   this._h = 50;
+  Enemy.prototype.setX.call(this,x);
+  Enemy.prototype.setY.call(this,y);
   this.image = image;
 };
 
@@ -14,4 +17,10 @@ Enemy.prototype.getAttr = function() {
     'x': this.x,
     'y': this.y
   };
+};
+Enemy.prototype.setX = function(x) {
+  this.x = x > this._canvasW - this._w ? this._canvasW - this._w : x;
+};
+Enemy.prototype.setY = function(y) {
+  this.y = y > this._canvasH - this._h ? this._canvasH - this._h : y;
 };
--- watchout.js
+++ watchout.js
@@ -8,14 +8,21 @@ var svg = d3.select('body').append('svg');
 svg.attr('height', h).attr('width', w);
 
 for (var i = 0; i < 10; i++) {
-  enemies.push(new Enemy(Math.random() * w, Math.random() * h, 'asteroid.png'));
-  svg.append('image').attr(enemies[i].getAttr());
+  enemies.push(new Enemy(i, Math.random() * w, Math.random() * h, 'asteroid.png'));
+  svg.append('image')
+    .datum(enemies[i])
+    .attr(enemies[i].getAttr());
 }
 
+var refresh = function () {
+  for (var i = 0; i < enemies.length; i++){
+    enemies[i].setX(Math.random() * w);
+    enemies[i].setY(Math.random() * h);
+  }
+  svg.selectAll('image')
+    .data(enemies, function(d) { return d.id; })
+    .attr('x', function(d) { return d.x; })
+    .attr('y', function(d) { return d.y; });
+};
 
-
-// svg.append('image')
-
-// var insertEnemy = function() {
-
-// };
+var motion = setInterval(refresh, 1000);
